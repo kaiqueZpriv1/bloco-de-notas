751e67c3e29f370521f556c6acd6d9ec076fe237
Kaique Ferreira <kakamelo141205@gmail.com>
2023-06-28 22:13:35 -0300

    add função de remove

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,56 @@
-const titleElement = document.getElementById('title');
-const notaElement = document.getElementById('nota');
-const salvarElement = document.getElementById('salvar');
-const commentsElements = document.getElementById('comments');
+const inputNota = document.getElementById("noteInput");
+const buttonSave = document.getElementById("saveButton");
+const listNote = document.getElementById("noteList");
 
-function addComments() {
-	commentsElements.innerHTML += `
-	<div id="novaNota"> 
-		<h3 id="titleComments" class="titleC">${titleElement.value}</h3>
-		<p id="notaComments" class="notaC">${notaElement.value}</p>
-		<div id="action" class="action">
-			<button id="btn-fav">&#9733</button>
-			<button id="btn-excluir">&#128465</button>
-		</div>
-	</div>
-	`;
+buttonSave.addEventListener("click", () => {
+  if (inputNota.value === "") {
+    console.log("Error, the text field is empty", Error);
+  } else {
+    const localNota = inputNota.value;
+    const timestamp = Date.now();
+    const storageKey = `note_${timestamp}`;
+    listNote.innerHTML += `
+      <div class="note-item">
+        <p id="nota">${inputNota.value}</p>
+        <div class="btns-actions">
+          <button class="btn-excluir" id="${storageKey}">&#128465;</button>
+          <button class="btn-favoritar" id="favoritar">&#10030;</button>
+        </div>
+      </div>
+    `;
+    localStorage.setItem(storageKey, localNota);
+    inputNota.value = ""; // Clear the input field after saving
+  }
+});
+
+// Add event listener to the parent container of the notes for delegation
+listNote.addEventListener("click", (event) => {
+  if (event.target.classList.contains("btn-excluir")) {
+    const noteElement = event.target.parentNode.parentNode;
+    const noteKey = event.target.id;
+    localStorage.removeItem(noteKey);
+    noteElement.remove();
+  }
+});
+
+// Retrieve and display the stored notes
+function populateNotes() {
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (key.startsWith("note_")) {
+      const note = localStorage.getItem(key);
+      const storageKey = key;
+      listNote.innerHTML += `
+        <div class="note-item">
+          <p id="nota">${note}</p>
+          <div class="btns-actions">
+            <button class="btn-excluir" id="${storageKey}">&#128465;</button>
+            <button class="btn-favoritar" id="favoritar">&#10030;</button>
+          </div>
+        </div>
+      `;
+    }
+  }
 }
-addComments();
 
-
-
-salvarElement.addEventListener('click', () => {
-	if (titleElement.value === "" || notaElement.value === "") {
-  	  console.log("Erro: Um ou ambos os inputs estão vazios!");
-  	} else {
-  	  console.log("Concluído!");
-	  addComments();
-	  titleElement.value = ""
-	  notaElement.value = ""
-  	}
-})
+populateNotes();
